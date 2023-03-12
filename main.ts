3448b8bb2787673ef749add18bf652fcd90c8761
magggieyang171 <116053282+magggieyang171@users.noreply.github.com>
2023-03-12 10:33:24 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Meh)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
+})
+basic.forever(function () {
+	
+})
